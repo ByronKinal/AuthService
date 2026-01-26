2e094f3436eb18251c18e62c7ecaee7cb541f91f
rcana-2021639 <rcana-2021639@kinal.edu.gt>
2026-01-26 17:17:03 -0600

    feat: Add error codes and token generation services
    - Introduced ErrorCodes class to manage application-specific error codes.
    - Implemented TokenGeneratorService for generating secure email verification and password reset tokens.
    - Added UuidGenerator for generating short UUIDs using a custom alphabet.
    - Updated project dependencies to include Konscious.Security.Cryptography.Argon2 and related packages.
    - Modified project paths in various configuration files to reflect new directory structure.

--- src/AuthServiceIN6BV.Application/obj/Debug/net8.0/AuthServiceIN6BV.Application.AssemblyInfo.cs
+++ src/AuthServiceIN6BV.Application/obj/Debug/net8.0/AuthServiceIN6BV.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServiceIN6BV.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b2a596b12464f6bb67f25811cbde461fc2ce2f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServiceIN6BV.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServiceIN6BV.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
